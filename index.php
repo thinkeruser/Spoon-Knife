fa61648da19acde1539531067ffec17b199733a3
asion <songhuxiang@sohu.com>
2015-03-04 15:12:41 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/3/4
+ * Time: 10:05
+ */
+//中文
+function dropRepeat($arr){
+    $arr = array(12,12,3123,1231);
+    $arr = array_sum($arr);
+
+    var_dump($arr);
+
+    function($path){
+        if(file_exists($path)){
+
+        }
+    }
+
+
+
